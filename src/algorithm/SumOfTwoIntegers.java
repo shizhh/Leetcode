70ead47a46afa36bde65971fb8a20a4781d62708
zhenhui <szh@zhenhui-mac.local>
2016-07-30 20:00:28 +0800

    两数相加，不用+、-

--- src/algorithm/SumOfTwoIntegers.java
+++ src/algorithm/SumOfTwoIntegers.java
@@ -12,7 +12,27 @@ public class SumOfTwoIntegers {
         }
         return a;
     }
+    // 减法 a-b
+    public int getSubtract(int a, int b){
+        b = ~b + 1;
+        while(b != 0){
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+
+    public int getSubtract2(int a, int b){
+        while (b != 0){
+            int borrow = (~a) & b;
+            a = a ^ b;
+            b = borrow << 1;
+        }
+        return a;
+    }
     public static void main(String[] args){
         System.out.println(new SumOfTwoIntegers().getSum(-8,-1));
+        System.out.println(new SumOfTwoIntegers().getSubtract(2,3));
     }
 }
